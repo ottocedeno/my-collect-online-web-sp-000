18d935a6ce9fb08564e27b98a271ffbe77793280
Otto Cedeno <otto@ottostacos.com>
2019-11-10 03:46:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i = 0
+  new_collection = Array.new
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  return new_collection
+end
 
+names = ["Otto", "Charlotte", "Indy"]
 
+print my_collect(names) {|name| "Hi there, #{name}!"}
